f6855f611d2b734dd6cba2ca6c40aa2963f179b1
rrhhdatospruebas-dot <rrhhdatospruebas@gmail.com>
2026-08-17 00:18:59 -0300

    Add eliminarDataset function to delete datasets

--- db.js
+++ db.js
@@ -86,6 +86,10 @@ async function setDataset(clave, valor) {
   );
 }
 
+async function eliminarDataset(clave) {
+  await pool.query('DELETE FROM datasets WHERE clave = $1', [clave]);
+}
+
 async function buscarUsuario(usuario) {
   const { rows } = await pool.query('SELECT * FROM usuarios WHERE usuario = $1', [usuario]);
   return rows[0] || null;
@@ -123,6 +127,7 @@ module.exports = {
   setConfig,
   getDataset,
   setDataset,
+  eliminarDataset,
   buscarUsuario,
   listarUsuarios,
   crearUsuario,
